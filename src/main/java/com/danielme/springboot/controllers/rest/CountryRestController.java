502b5485d1a4be4a04bf0233dfc8bf9bbdbeffd7
dani <danielme_com@yahoo.com>
2020-11-02 12:33:57 +0100

    eliminar método no usado

--- src/main/java/com/danielme/springboot/controllers/rest/CountryRestController.java
+++ src/main/java/com/danielme/springboot/controllers/rest/CountryRestController.java
@@ -1,23 +1,10 @@
 package com.danielme.springboot.controllers.rest;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
+import com.danielme.springboot.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.danielme.springboot.model.Country;
-import com.danielme.springboot.services.CountryService;
 
 @RestController
 @RequestMapping(CountryRestController.COUNTRY_RESOURCE)
@@ -25,21 +12,6 @@ public class CountryRestController {
 
     public static final String COUNTRY_RESOURCE = "/api/country";
 
-    private final CountryService countryService;
-
-    public CountryRestController(CountryService countryService) {
-        this.countryService = countryService;
-    }
-
-    @GetMapping(value = "/{id}/")
-    public ResponseEntity<Country> getById(@PathVariable("id") Integer id) {
-        Optional<Country> country = countryService.findById(id);
-        if (country.isPresent()) {
-            return new ResponseEntity<>(country.get(), HttpStatus.OK);
-        }
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-    }
-    
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void add(@RequestBody @Valid Country country) {
