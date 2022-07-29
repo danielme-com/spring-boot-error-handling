e47e95208bfc5fd09a0919d594294a8a40734c74
dani <danielme_com@yahoo.com>
2022-07-29 18:30:35 +0200

    cambiar nombre main

--- src/main/java/com/danielme/springboot/ErrorsApp.java
+++ src/main/java/com/danielme/springboot/ErrorsApp.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CountryApp extends SpringBootServletInitializer {
+public class ErrorsApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(CountryApp.class, args);
+        SpringApplication.run(ErrorsApp.class, args);
     }
 
 }
