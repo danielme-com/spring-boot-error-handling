5ed2f39a41dc28ed15c0a3cf97710954c11d37ee
dani <danielme_com@yahoo.com>
2022-10-14 19:32:36 +0200

    quitar id de Country

--- src/main/java/com/danielme/springboot/model/Country.java
+++ src/main/java/com/danielme/springboot/model/Country.java
@@ -6,30 +6,13 @@ import javax.validation.constraints.NotNull;
 
 public class Country {
 
-    private Integer id;
-    
     @NotNull
     private String name;
-    
+
     @NotNull
     @Min(1)
     @Max(2000000000)
     private Integer population;
-    
-    public Country() {
-        super();
-    }
-
-    public Country(Integer id, String name, Integer population) {
-        this.id = id;
-        this.name = name;
-        this.population = population;
-    }
-    
-    
-    public Integer getId() {
-        return id;
-    }
 
     public String getName() {
         return name;
@@ -37,10 +20,6 @@ public class Country {
 
     public Integer getPopulation() {
         return population;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public void setName(String name) {
