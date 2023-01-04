e9a0b0cb5b9798965bc267a0837a7f4e9b003511
dani <danielme_com@yahoo.com>
2023-01-04 23:04:41 +0100

    falta @ResponseStatus en un @ExceptionHandler

--- src/main/java/com/danielme/springboot/controllers/errors/advices/ErrorJspControllerAdvice.java
+++ src/main/java/com/danielme/springboot/controllers/errors/advices/ErrorJspControllerAdvice.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
  
@@ -18,6 +19,7 @@ public class ErrorJspControllerAdvice {
     private static final Logger logger = LoggerFactory.getLogger(ErrorJspControllerAdvice.class);
  
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleException(Exception ex, HttpServletRequest request, Model model) {
         logger.info("executing exception handler (web)");
  
